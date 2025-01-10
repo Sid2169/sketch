74088a4c952f2c3a9764b1460413be4b1683e59c
Sid2169 <siddharthasingh21109572@gmail.com>
2025-01-10 23:00:27 +0530

    Add initial canvas style
    Initial canvas style set and body background color.

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-const gridDimension = 128;
-const grid = document.createElement('div');
-grid.classList.add('grid');
+let gridDimension = 16;
+createNewGrid(gridDimension);
 
-for (let i = 0; i < gridDimension; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.classList.add('grid-row');
-    for (let j = 0; j < gridDimension; j++) {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        gridRow.appendChild(gridCell);
+
+//Function to create a new grid
+function createNewGrid(size) {
+     // Remove the old grid if it exists
+     const oldGrid = document.getElementById('grid');
+     if (oldGrid) {
+         document.querySelector('body').removeChild(oldGrid);
+     }
+    
+    // Create a new grid
+    const grid = document.createElement('div');
+    grid.id = 'grid';
+    let isDrawing = false;
+    document.body.addEventListener('mousedown', () => {
+        isDrawing = true;
+    });
+    document.body.addEventListener('mouseup', () => {
+        isDrawing = false;
+    });
+    for (let i = 0; i < size; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        for (let j = 0; j < size; j++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('grid-cell');
+            gridCell.addEventListener('mouseover', () => {
+                if (isDrawing) {
+                    gridCell.style.backgroundColor = 'black';
+                }
+            });
+            gridCell.addEventListener('mousedown', () => {
+                gridCell.style.backgroundColor = 'black';
+            });
+            gridRow.appendChild(gridCell);
+        }
+        grid.appendChild(gridRow);
     }
-    grid.appendChild(gridRow);
+    document.querySelector('body').appendChild(grid);
 }
-document.querySelector('body').appendChild(grid);
